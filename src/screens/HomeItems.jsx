8d42ca256199c67592fe43eba6ae56d046a8ab78
liptonzuma <solomonboateng231@gmail.com>
2021-10-25 17:41:17 +0000

    home

--- src/screens/HomeItems.jsx
+++ src/screens/HomeItems.jsx
@@ -31,16 +31,50 @@ const HomeItems = () => {
                             </Text>
                         </View>
                     </View>
-                    <View style={styles.item}>
 
-                </View>
+                    <View style={styles.watchWrapper}>
+                        <View style={styles.watcWrapper}>
+                            <View style={styles.watchContainer}>
+                            <Image 
+                                source={require("../../assets/images/watch.png")}
+                                resizeMode="contain"
+                                style={styles.watch}
+                            />
+                            </View>
+                            <Text style={styles.label}>
+                                Wrist watch
+                            </Text>
+                            <Text style={styles.label}>
+                                $120.00
+                            </Text>
+                        </View>
+                    </View>
+                    
+               
                 <View style={styles.item1}>
 
                 </View>
             </View> 
             <View style={styles.column2}>
-                <View style={styles.item1}>
+                <View style={styles.bagcontainer}>
+                <View style={styles.bagWrapper}>
+                            <Image 
+                                source={require("../../assets/images/bagpack.png")}
+                                resizeMode="contain"
+                                style={styles.bag}
+                            />
 
+                </View>
+                <View style={{
+                    paddingTop:15
+                }}>
+                <Text style={styles.label}>
+                    Backpack
+                </Text>
+                <Text style={styles.label}>
+                    $15.00
+                </Text>
+                </View>
                 </View>
                 <View style={styles.item1}>
                 <Text style={styles.new}>New</Text>
@@ -87,9 +121,25 @@ const styles = StyleSheet.create({
         borderRadius:10,
         marginBottom:10
     },
+    watcWrapper:{
+        width:'95%',
+        backgroundColor:'#e8d776',
+        height:200,
+        borderRadius:10,
+        marginBottom:10
+    },
     item1:{
         width:'95%',
         backgroundColor:'green',
+        height:230,
+        borderRadius:10,
+        marginBottom:10
+
+
+    },
+    bagcontainer:{
+        width:'95%',
+        backgroundColor:'#8ad9ed',
         height:230,
         borderRadius:10,
         marginBottom:10
@@ -114,12 +164,31 @@ const styles = StyleSheet.create({
         height:150,
         marginTop:-50
     },
+    watch:{
+        width:120,
+        height:120,
+        marginTop:10
+    },
     label:{
         fontSize:17,
         textAlign:'left',
         fontWeight:'500',
         paddingLeft:10
-        }
+     },
+     bag:{
+        width:150,
+        height:150,
+     },
+     bagWrapper:{
+         justifyContent:'center',
+         alignItems:'center'
+     },
+     watchContainer:{
+         justifyContent:'center',
+         alignItems:'center',
+         marginBottom:15
+     },
+
 
 
 })
