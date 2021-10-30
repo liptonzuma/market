0afefbe4867bd4e74961222e3a532a31eb00cda4
liptonzuma <solomonboateng231@gmail.com>
2021-10-30 00:48:42 +0000

    products

--- src/screens/ProductList.jsx
+++ src/screens/ProductList.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
 import AppBar from '../utils/AppBar';
+import Search from '../utils/Search'
+import data  from '../items.json'
+import { AntDesign } from '@expo/vector-icons';
+import { ScrollView } from 'react-native';
 
 const ProductList = () => {
     return (
         <View style={styles.container}>
             <AppBar screen="Shop"/>
-            <Text>ProductList</Text>
+            <View style={styles.Search}>
+            <Search/>
+            </View>
+            <View style ={styles.label}>
+                <Text style={styles.big}>Available Products</Text>
+                <Pressable style={styles.button}>
+                    <Text style={{color:'#383836'}}>sort by <AntDesign name="down" size={15} color="#383836" /></Text>
+                </Pressable>
+            </View>
+            <ScrollView>
+                
+            </ScrollView>
         </View>
     )
 }
@@ -15,8 +30,24 @@ export default ProductList;
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
-        justifyContent:'center',
+        flex:1,  
+    },
+    Search:{
+        marginTop:80
+    },
+    label:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingHorizontal:10,
         alignItems:'center'
+    },
+    big:{
+        fontSize:20,
+        fontWeight:'700'
+    },
+    button:{
+        backgroundColor:'white',
+        padding:7
     }
+
 })
