aa0190141586fd19de040dbb62c02dc9c287adc3
liptonzuma <solomonboateng231@gmail.com>
2021-11-03 17:44:16 +0000

    redux

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -4,7 +4,28 @@ const initialState ={
 }
 
 const reducer =(state=initialState,action)=>{
-    return state;
+    switch(action.type){
+        case 'ADD_TO_CART':
+            return {
+                ...state,
+                cart: state.cart.concat(action.payload)
+            }
+        case 'REMOVE_FROM_CART':
+            return{
+                ...state,
+                cart:state.cart.filter(e=> e !== payload)
+            }
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorite:state.favorite.concat(action.payload)
+            }
+        case 'REMOVE_FROM_FAVORITE':
+            return{
+                ...state,
+                favorite:state.favorite.filter(e=> e !== action.payload)
+            }
+    }
 }
 
 export default reducer;
